95e0c83756a1000be64539bd93f7b00fc2a71134
Sylvester Mbah <sylvestermbah90@gmail.com>
2023-04-11 09:35:36 -0700

    Add a function that displays the inputted values on the screen

--- script.js
+++ script.js
@@ -7,6 +7,17 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+function render() {
+    const libraryBook = document.querySelector('#library');
+    libraryBook.textContent = '';
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book = myLibrary[i];
+        let bookElement = document.createElement('p');
+        bookElement.textContent = `${book.title}`;
+        libraryBook.appendChild(bookElement);
+    }
+}
+
 function addBookToLibrary() {
   let title = document.querySelector('#title').value;
   let author = document.querySelector('#author').value;
@@ -14,7 +25,8 @@ function addBookToLibrary() {
   let read = document.querySelector('#read').checked;
 
   let newBook = new Book(title, author, pages, read);
-  console.log(newBook);
+  myLibrary.push(newBook);
+  render();
 }
 
 const bookBtn = document.querySelector('#new-book-btn');
