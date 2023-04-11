7934be0ef1b373145c6b6b2fe7dd9175dd57d22b
Sylvester Mbah <sylvestermbah90@gmail.com>
2023-04-11 08:38:55 -0700

    Initialise the object constructor and add an eventlistener that stores values inputted

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  let title = document.querySelector('#title').value;
+  let author = document.querySelector('#author').value;
+  let pages = document.querySelector('#pages').value;
+  let read = document.querySelector('#read').checked;
+
+  let newBook = new Book(title, author, pages, read);
+  console.log(newBook);
 }
 
 const bookBtn = document.querySelector('#new-book-btn');
@@ -13,4 +22,9 @@ const bookBtn = document.querySelector('#new-book-btn');
 bookBtn.addEventListener('click', function() {
     let bookForm = document.querySelector('#book-form');
     bookForm.style.display = 'block';
+})
+
+document.querySelector('#book-form').addEventListener('submit', function() {
+    event.preventDefault();
+    addBookToLibrary();
 })
